05108fda9c8030cf081374830055df7e2ee85d8a
sstruct <sstruct@gmail.com>
2018-02-10 17:28:00 +0800

    About prop-types

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import "./index.css";
 
 const names = ["Adam", "Eva", "God", "Snake"];
@@ -14,8 +15,12 @@ export default class App extends React.Component {
     );
   }
 }
+ReactDOM.render(<App />, document.getElementById("app"));
 
-// About Pure function
+/**
+ * About Pure function
+ * react render() function must be pure, so it can get its most efficiency
+ */
 
 // pure
 var friends = ["Ryan", "Michael", "Dan"];
@@ -28,4 +33,103 @@ friends.splice(0, 1); // ["Ryan"]
 friends.splice(0, 1); // ["Michael"]
 friends.splice(0, 1); // ["Dan"]
 
-ReactDOM.render(<App />, document.getElementById("app"));
+/**
+ * About PropTypes
+ * ref: https://www.npmjs.com/package/prop-types
+ */
+
+App.propTypes = {
+  // You can declare that a prop is a specific JS primitive. By default, these
+  // are all optional.
+  optionalArray: PropTypes.array,
+  optionalBool: PropTypes.bool,
+  optionalFunc: PropTypes.func,
+  optionalNumber: PropTypes.number,
+  optionalObject: PropTypes.object,
+  optionalString: PropTypes.string,
+  optionalSymbol: PropTypes.symbol,
+
+  // Anything that can be rendered: numbers, strings, elements or an array
+  // (or fragment) containing these types.
+  optionalNode: PropTypes.node,
+
+  // A React element.
+  // ⚠️ With PropTypes.element you can specify that only a single child can be
+  // passed to a component as children.
+  optionalElement: PropTypes.element,
+
+  // You can also declare that a prop is an instance of a class. This uses
+  // JS's instanceof operator.
+  optionalMessage: PropTypes.instanceOf(Message),
+
+  // You can ensure that your prop is limited to specific values by treating
+  // it as an enum.
+  optionalEnum: PropTypes.oneOf(["News", "Photos"]),
+
+  // An object that could be one of many types
+  optionalUnion: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.instanceOf(Message)
+  ]),
+
+  // An array of a certain type
+  optionalArrayOf: PropTypes.arrayOf(PropTypes.number),
+
+  // An object with property values of a certain type
+  optionalObjectOf: PropTypes.objectOf(PropTypes.number),
+
+  // An object taking on a particular shape
+  optionalObjectWithShape: PropTypes.shape({
+    color: PropTypes.string,
+    fontSize: PropTypes.number
+  }),
+
+  // You can chain any of the above with `isRequired` to make sure a warning
+  // is shown if the prop isn't provided.
+  requiredFunc: PropTypes.func.isRequired,
+
+  // A value of any data type
+  requiredAny: PropTypes.any.isRequired,
+
+  // NOTE: 大部分情况下，没有这个需求；暂时不做实际编码
+  // You can also specify a custom validator. It should return an Error
+  // object if the validation fails. Don't `console.warn` or throw, as this
+  // won't work inside `oneOfType`.
+  customProp: function(props, propName, componentName) {
+    if (!/matchme/.test(props[propName])) {
+      return new Error(
+        "Invalid prop `" +
+          propName +
+          "` supplied to" +
+          " `" +
+          componentName +
+          "`. Validation failed."
+      );
+    }
+  },
+
+  // You can also supply a custom validator to `arrayOf` and `objectOf`.
+  // It should return an Error object if the validation fails. The validator
+  // will be called for each key in the array or object. The first two
+  // arguments of the validator are the array or object itself, and the
+  // current item's key.
+  customArrayProp: PropTypes.arrayOf(function(
+    propValue,
+    key,
+    componentName,
+    location,
+    propFullName
+  ) {
+    if (!/matchme/.test(propValue[key])) {
+      return new Error(
+        "Invalid prop `" +
+          propFullName +
+          "` supplied to" +
+          " `" +
+          componentName +
+          "`. Validation failed."
+      );
+    }
+  })
+};
