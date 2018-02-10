219796a3e2662c9681e88bd250011aeffaec484b
sstruct <sstruct@gmail.com>
2018-02-10 17:14:53 +0800

    Pure functions
    Pure functions doesn't:
     * Pure functions doesn't:
     * depend on variables outside its scope
     * modify variables outside its scope
     * make Ajax requests
     * manipulate the DOM
    
    React render() function must be pure, so it can get its most efficiency
    
    NOTE: when using .map function, use unique `key` can help react save lots of work

--- app/index.js
+++ app/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const names = ["Adam", "Eva", "God", "Snake"];
+
 export default class App extends React.Component {
   render() {
-    return <div>hello world</div>;
+    return (
+      <div>
+        Names:
+        <ul>{names.map(o => <li key={o}>{o}</li>)}</ul>
+      </div>
+    );
   }
 }
+
+// About Pure function
+
+// pure
+var friends = ["Ryan", "Michael", "Dan"];
+friends.slice(0, 1); // 'Ryan'
+friends.slice(0, 1); // 'Ryan'
+friends.slice(0, 1); // 'Ryan'
+
+// unpure
+friends.splice(0, 1); // ["Ryan"]
+friends.splice(0, 1); // ["Michael"]
+friends.splice(0, 1); // ["Dan"]
 
 ReactDOM.render(<App />, document.getElementById("app"));
